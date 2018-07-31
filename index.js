f413872f038b089614d3269bdc098f5a4aebbd39
pablohenrique <pablo@softbox.com.br>
2018-07-31 17:55:57 -0300

    fix(rules): enforced good practices and fixed missing rules

--- index.js
+++ index.js
@@ -7,14 +7,230 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'accessor-pairs': 'off',
+
+    'array-callback-return': ['error', { allowImplicit: true }],
+
+    'block-scoped-var': 'error',
+
+    complexity: ['off', 11],
+
+    'class-methods-use-this': ['error', {
+      exceptMethods: [],
+    }],
+
+    'consistent-return': 'error',
+
+    curly: ['error', 'multi-line'],
+
+    'default-case': ['error', { commentPattern: '^no default$' }],
+
+    'dot-notation': ['error', { allowKeywords: true }],
+
+    'dot-location': ['error', 'property'],
+
+    eqeqeq: ['error', 'always', { null: 'ignore' }],
+
+    'guard-for-in': 'error',
+
+    'max-classes-per-file': ['off', 1],
+
+    'no-alert': 'warn',
+
+    'no-caller': 'error',
+
+    'no-case-declarations': 'error',
+
+    'no-div-regex': 'off',
+
+    'no-else-return': ['error', { allowElseIf: false }],
+
+    'no-empty-function': ['error', {
+      allow: [
+        'arrowFunctions',
+        'functions',
+        'methods',
+      ]
+    }],
+
+    'no-empty-pattern': 'error',
+
+    'no-eq-null': 'off',
+
+    'no-eval': 'error',
+
+    'no-extend-native': 'error',
+
+    'no-extra-bind': 'error',
+
+    'no-extra-label': 'error',
+
+    'no-fallthrough': 'error',
+
+    'no-floating-decimal': 'error',
+
+    'no-global-assign': ['error', { exceptions: [] }],
+    'no-native-reassign': 'off',
+
+    'no-implicit-coercion': ['off', {
+      boolean: false,
+      number: true,
+      string: true,
+      allow: [],
+    }],
+
+    'no-implicit-globals': 'off',
+
+    'no-implied-eval': 'error',
+
+    'no-invalid-this': 'off',
+
+    'no-iterator': 'error',
+
+    'no-labels': ['error', { allowLoop: false, allowSwitch: false }],
+
+    'no-lone-blocks': 'error',
+
+    'no-loop-func': 'error',
+
+    'no-magic-numbers': ['off', {
+      ignore: [],
+      ignoreArrayIndexes: true,
+      enforceConst: true,
+      detectObjects: false,
+    }],
+
+    'no-multi-spaces': ['error', {
+      ignoreEOLComments: false,
+    }],
+
+    'no-multi-str': 'error',
+
+    'no-new': 'error',
+
+    'no-new-func': 'error',
+
+    'no-new-wrappers': 'error',
+
+    'no-octal': 'error',
+
+    'no-octal-escape': 'error',
+
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+      ]
+    }],
+
+    'no-proto': 'error',
+
+    'no-redeclare': 'error',
+
+    'no-restricted-properties': ['error', {
+      object: 'arguments',
+      property: 'callee',
+      message: 'arguments.callee is deprecated',
+    }, {
+        object: 'global',
+        property: 'isFinite',
+        message: 'Please use Number.isFinite instead',
+      }, {
+        object: 'self',
+        property: 'isFinite',
+        message: 'Please use Number.isFinite instead',
+      }, {
+        object: 'window',
+        property: 'isFinite',
+        message: 'Please use Number.isFinite instead',
+      }, {
+        object: 'global',
+        property: 'isNaN',
+        message: 'Please use Number.isNaN instead',
+      }, {
+        object: 'self',
+        property: 'isNaN',
+        message: 'Please use Number.isNaN instead',
+      }, {
+        object: 'window',
+        property: 'isNaN',
+        message: 'Please use Number.isNaN instead',
+      }, {
+        property: '__defineGetter__',
+        message: 'Please use Object.defineProperty instead.',
+      }, {
+        property: '__defineSetter__',
+        message: 'Please use Object.defineProperty instead.',
+      }, {
+        object: 'Math',
+        property: 'pow',
+        message: 'Use the exponentiation operator (**) instead.',
+      }],
+
+    'no-return-assign': ['error', 'always'],
+
+    'no-return-await': 'error',
+
+    'no-script-url': 'error',
+
+    'no-self-assign': ['error', {
+      props: false,
+    }],
+
+    'no-self-compare': 'error',
+
+    'no-sequences': 'error',
+
+    'no-throw-literal': 'error',
+
+    'no-unmodified-loop-condition': 'off',
+
+    'no-unused-expressions': ['error', {
+      allowShortCircuit: false,
+      allowTernary: false,
+      allowTaggedTemplates: false,
+    }],
+
+    'no-unused-labels': 'error',
+
+    'no-useless-call': 'off',
+
+    'no-useless-concat': 'error',
+
+    'no-useless-escape': 'error',
+
+    'no-useless-return': 'error',
+
+    'no-void': 'error',
+
+    'no-warning-comments': ['off', { terms: ['todo', 'fixme', 'xxx'], location: 'start' }],
+
+    'no-with': 'error',
+
+    'prefer-promise-reject-errors': ['error', { allowEmptyReject: true }],
+
+    radix: 'error',
+
+    'require-await': 'off',
+
+    'vars-on-top': 'error',
+
+    'wrap-iife': ['error', 'outside', { functionPrototypeMethods: false }],
+
+    yoda: 'error',
+
     'prefer-destructuring': [
       'error',
       {
-        'array': true,
-        'object': true
-      },
-      {
-        'enforceForRenamedProperties': false
+        'VariableDeclarator': {
+          'array': false,
+          'object': true,
+        },
+        'AssignmentExpression': {
+          'array': true,
+          'object': true,
+        },
+      }, {
+        'enforceForRenamedProperties': false,
       }
     ],
     'quotes': [
@@ -37,9 +253,9 @@ module.exports = {
     ],
     'wrap-iife': [
       'error',
-      'outside',
+      'any',
       {
-        functionPrototypeMethods: false
+        'functionPrototypeMethods': true
       }
     ],
     'prefer-rest-params': 'error',
@@ -48,12 +264,19 @@ module.exports = {
       'error',
       'always'
     ],
+    'comma-dangle': ['error', {
+      'arrays': 'never',
+      'objects': 'never',
+      'imports': 'never',
+      'exports': 'never',
+      'functions': 'never'
+    }],
     'space-before-blocks': 'error',
     'no-param-reassign': 'error',
     'prefer-spread': 'error',
     'function-paren-newline': [
       'error',
-      'always'
+      'consistent'
     ],
     'prefer-arrow-callback': 'error',
     'arrow-spacing': 'error',
@@ -84,10 +307,16 @@ module.exports = {
     ],
     'dot-notation': 'error',
     'no-undef': 'error',
-    'prefer-const': 'error',
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: true,
+      }
+    ],
     'one-var': [
       'error',
-      'always'
+      'never'
     ],
     'no-multi-assign': 'error',
     'no-plusplus': 'error',
