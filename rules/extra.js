52cfb065bf75d57c01eb521c6446d4811e5b9025
pablohenrique <pablo@softbox.com.br>
2018-08-08 09:24:22 -0300

    feat(react/vue): added react and vue lint

---
+++ rules/extra.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "sort-imports-es6-autofix/sort-imports-es6": [
+    2,
+    {
+      "ignoreCase": false,
+      "ignoreMemberSort": false,
+      "memberSyntaxSortOrder": ["none", "all", "multiple", "single"]
+    }
+  ],
+  "class-methods-use-this": 0
+}
