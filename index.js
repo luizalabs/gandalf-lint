57db5066a63adbb14b496e08d3ad93ff911db002
Lucas Vilaboim <lucas@vilaboim.com>
2019-03-28 11:58:25 -0300

    feat(dependencies): move to peerDependencies

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 module.exports = {
-  extends: ["eslint-config-standard", "./rules/gandalf"].map(require.resolve),
-  plugins: ["sort-imports-es6-autofix"],
+  extends: [
+    'standard',
+    './rules/gandalf'
+  ].map(require.resolve),
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module'
   },
   rules: {
-    "class-methods-use-this": 0
+    'class-methods-use-this': 0
   }
-};
+}
