0a6cf753cefa1aa300dd07fd03b98724d3cb75b0
iTzMeRafa <rafael97@gmx.de>
2019-10-02 12:44:14 +0200

    added double quotes of rule to single quotes

--- rules/gandalf.js
+++ rules/gandalf.js
@@ -184,7 +184,7 @@ module.exports = {
       'exports': 'never',
       'functions': 'never'
     }],
-    "max-params": ['error', 3],
+    'max-params': ['error', 3],
     'space-before-blocks': 'error',
     'no-param-reassign': 'error',
     'prefer-spread': 'error',
